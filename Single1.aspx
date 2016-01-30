357de5e1c98c038b403f202403d13e5d03f56518
Varen Fang <varen.fang@gmail.com>
2016-01-30 09:57:16 +0800

    Single file upload

--- Single1.aspx
+++ Single1.aspx
@@ -6,15 +6,30 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
 </head>
 <body>
+
+    <h3>
+        Single file, ASP.NET web control, Save to D:/upload/
+    </h3>
+
     <form id="form1" runat="server">
-    <div>
-        <h3>
-            Single file, ASP.NET web control, Save to D:/download/
-        </h3>
-    
-    </div>
+        <asp:FileUpload ID="fileupload1" runat="server" />
+        <br />
+        <br />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click"/>
+
+        <input type="hidden" runat="server" id="lblMsg" value="{message = 'init complete'}"/>
     </form>
+
+    <script>
+        $(document).ready(function () {
+            console.log(JSON.parse($("#lblMsg").val()));
+        });
+    </script>
+
 </body>
 </html>
--- Single1.aspx.cs
+++ Single1.aspx.cs
@@ -1,14 +1,40 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 
 public partial class FileUpload1 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        if (fileupload1.HasFile)
+        {
+
+            var outputObj = new {
+                fileName = fileupload1.FileName,
+                ext = Path.GetExtension(fileupload1.FileName),
+                mimeType = fileupload1.PostedFile.ContentType,
+                size = fileupload1.PostedFile.ContentLength + " Bytes",
+            };
+
+            lblMsg.Value = jss.Serialize(outputObj);
+
+            fileupload1.SaveAs(@"D:\upload\" + fileupload1.FileName);
+        }
+        else
+        {
+            lblMsg.Value = jss.Serialize(new { message = "No file" });
+        }
     }
 }
